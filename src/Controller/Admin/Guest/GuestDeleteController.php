f9359dfc11bd25293f2a04ba42413a34133102e8
Silf2 <sylvain.guenet1@gmail.com>
2024-09-10 14:21:22 +0200

    Fixes #12 : Admin can delete Guests

---
+++ src/Controller/Admin/Guest/GuestDeleteController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Controller\Admin\Guest;
+
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpKernel\Attribute\AsController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\RouterInterface;
+
+#[AsController]
+final class GuestDeleteController
+{
+    public function __construct(
+        private UserRepository $userRepository,
+        private EntityManagerInterface $entityManager,
+        private RouterInterface $router,
+    )
+    {}
+
+    #[Route("/admin/guest/delete/{id}", name:"app_admin_guest_delete")]
+    public function __invoke(int $id): RedirectResponse
+    {
+        $user = $this->userRepository->findOneBy(["id" => $id]);
+
+        if(!$user){
+            throw new \Exception("L'utilisateur que vous essayez de supprimer n'existe pas.");
+        }
+
+        $this->entityManager->remove($user);
+        $this->entityManager->flush();
+
+        return new RedirectResponse($this->router->generate('app_admin_guest_index'));
+    }
+}
