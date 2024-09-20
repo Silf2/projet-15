e6b91b1627c70cf97d9662ba0d07a92a92c7a82d
Silf2 <sylvain.guenet1@gmail.com>
2024-09-20 13:28:51 +0200

    Fixes #15 : Creating dataFixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,9 +3,12 @@
 namespace App\DataFixtures;
 
 use App\Entity\User;
+use App\Entity\Album;
+use App\Entity\Media;
 use Doctrine\Persistence\ObjectManager;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Faker\Factory;
 
 class AppFixtures extends Fixture
 {
@@ -13,17 +16,53 @@ class AppFixtures extends Fixture
         private UserPasswordHasherInterface $userPasswordHasher
     )
     {}
-    
+
     public function load(ObjectManager $manager): void
     {
-        $user = new User();
-        $user->setName('Ina');
-        $user->setEmail('Ina@gmail.com');
-        $user->setDescription('');
-        $user->setRoles(["ROLE_ADMIN"]);
-        $user->setPassword($this->userPasswordHasher->hashPassword($user, "password"));
+        $faker = Factory::create();
 
-        $manager->persist($user);
+        // Création de l'utilisateur admin
+        $admin = new User();
+        $admin->setName('Ina');
+        $admin->setEmail('Ina@gmail.com');
+        $admin->setDescription('');
+        $admin->setRoles(["ROLE_ADMIN"]);
+        $admin->setPassword($this->userPasswordHasher->hashPassword($admin, "password"));
+        $manager->persist($admin);
+
+        // Création d'utilisateurs aléatoires
+        $users = [];
+        for ($i = 0; $i < 10; $i++) {
+            $user = new User();
+            $user->setName($faker->name);
+            $user->setEmail($faker->unique()->email);
+            $user->setDescription($faker->sentence);
+            $user->setRoles(["ROLE_USER"]);
+            $user->setPassword($this->userPasswordHasher->hashPassword($user, "password"));
+            $manager->persist($user);
+            $users[] = $user;
+        }
+
+        // Création d'albums
+        $albums = [];
+        for ($i = 0; $i < 5; $i++) {
+            $album = new Album();
+            $album->setName($faker->word . ' Album');
+            $manager->persist($album);
+            $albums[] = $album;
+        }
+
+        // Création de médias (images)
+        for ($i = 0; $i < 20; $i++) {
+            $media = new Media();
+            $media->setTitle($faker->sentence(3));
+            $media->setPath($faker->imageUrl());
+            $media->setUser($faker->randomElement($users));
+            $media->setAlbum($faker->randomElement($albums));
+            $manager->persist($media);
+        }
+
+        // Flush pour sauvegarder toutes les entités
         $manager->flush();
     }
 }
