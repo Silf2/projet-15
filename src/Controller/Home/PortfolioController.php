1739ed260dd6b45b5f287923256cb35ddcb468ef
Silf2 <sylvain.guenet1@gmail.com>
2024-09-20 14:42:52 +0200

    Remove of unnecessary dependencies

--- src/Controller/Home/PortfolioController.php
+++ src/Controller/Home/PortfolioController.php
@@ -2,13 +2,9 @@
 
 namespace App\Controller\Home;
 
-use App\Entity\Album;
-use App\Entity\Media;
-use App\Entity\User;
 use App\Repository\AlbumRepository;
 use App\Repository\MediaRepository;
 use App\Repository\UserRepository;
-use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Attribute\AsController;
 use Symfony\Component\Routing\Annotation\Route;
