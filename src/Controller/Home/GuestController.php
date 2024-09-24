803ea85189638d5c68445a506ae0bc448afd195a
Silf2 <sylvain.guenet1@gmail.com>
2024-09-24 13:53:00 +0200

    (Patch) Using PaginatorInterface on guests Page

--- src/Controller/Home/GuestController.php
+++ src/Controller/Home/GuestController.php
@@ -3,6 +3,7 @@
 namespace App\Controller\Home;
 
 use App\Repository\UserRepository;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Attribute\AsController;
@@ -12,30 +13,26 @@ use Twig\Environment;
 #[AsController]
 final class GuestController
 {
-    private const GUESTS_PER_PAGE = 10;
-
     public function __construct(
         private Environment $twig,
-        private UserRepository $userRepository
+        private UserRepository $userRepository,
+        private PaginatorInterface $paginator
     )
     {}
 
     #[Route("/guests", name:"app_guests")]
     public function __invoke(Request $request): Response
     {
-        $page = max(1, $request->query->getInt('page', 1));
-        
-        $offset = ($page - 1) * self::GUESTS_PER_PAGE;
+        $queryBuilder = $this->userRepository->createQueryBuilder('u');
 
-        $guests = $this->userRepository->findBy([], null, self::GUESTS_PER_PAGE, $offset);
-
-        $totalGuests = $this->userRepository->count([]);
-        $totalPages = ceil($totalGuests / self::GUESTS_PER_PAGE);
+        $pagination = $this->paginator->paginate(
+            $queryBuilder,
+            $request->query->getInt('page', 1), 
+            10 
+        );
 
         $content = $this->twig->render('front/guests.html.twig', [
-            'guests' => $guests,
-            'currentPage' => $page,
-            'totalPages' => $totalPages
+            'pagination' => $pagination
         ]);
 
         return new Response($content);
